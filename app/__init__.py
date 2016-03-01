0277e341796bdc1e3b9cf02b3df94c874e8ccd67
Justen Morgan <justenamorgan@gmail.com>
2016-03-01 18:52:30 +0000

    update

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,13 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import os
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID, COMMON_PROVIDERS
+import sys
+import logging
 
 
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 app.config['SECRET_KEY'] = "this is a super secure key"
 app.config['OPENID_PROVIDERS'] = COMMON_PROVIDERS
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://cpszstmfjktcva:Lc6vcVRVH8KJz4itSa21w8_-RG@ec2-54-227-250-148.compute-1.amazonaws.com:5432/d78fr778jcg850"
